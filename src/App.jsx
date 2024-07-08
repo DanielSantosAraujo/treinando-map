5433dc697699830c341fe373146e8217c9246058
Daniel Santos de Araújo <dsaraujo718@gmail.com>
2024-07-08 16:27:15 -0300

    Adicionando página de carro separado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cadastro from './Pages/Cadastro';
 import Login from './Pages/Login';
 import './global.css';
 import Dashboard from './Pages/Dashboard';
+import Carrinho from './Pages/Carrinho';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/carro/:id' element={<Carrinho/>} />
       </Routes>
     </BrowserRouter>
   );
